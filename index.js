b7c6c1c220d01caeb218671af3ff81c449788cf9
johnthar929 <87939894+johnthar929@users.noreply.github.com>
2024-05-23 12:51:52 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
+const express = require('express');
+const http = require('http');
+const socketIo = require('socket.io');
 
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server);
+
+io.on('connection', (socket) => {
+    console.log('A user connected');
+    socket.on('disconnect', () => {
+        console.log('User disconnected');
+    });
+
+    // Handle custom events
+    socket.on('message', (msg) => {
+        console.log('Message received: ' + msg);
+        io.emit('message', msg); // Broadcast message to all clients
+    });
+});
+
+server.listen(3000, () => {
+    console.log('Listening on port 3000');
+});
